d19cb2de1fc69c7bbc8a670b5c27d9c4a0862be4
unknown <ladm9393@gmail.com>
2015-10-27 17:22:52 +0530

    custom circular view updated

--- held/app/src/main/java/com/held/customview/CircularImageView.java
+++ held/app/src/main/java/com/held/customview/CircularImageView.java
@@ -14,8 +14,14 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.widget.ImageView;
 
+import com.held.activity.R;
+
 public class CircularImageView extends ImageView {
 
+
+    private Paint paintCanvas=new Paint();
+    private Paint paintBorder=new Paint();
+    private float borderWidth;
     public CircularImageView(Context context) {
         super(context);
         Log.i("CustomView", "Circular image");
@@ -31,55 +37,103 @@ public class CircularImageView extends ImageView {
         Log.i("CustomView", "Circular image");
     }
 
+
     @Override
     protected void onDraw(Canvas canvas) {
-
+    int radius;
+        int w = getWidth(), h = getHeight();
+        radius=Math.min(w, h);
         Drawable drawable = getDrawable();
 
         if (drawable == null) {
+            drawRing(canvas,radius);
             return;
         }
 
         if (getWidth() == 0 || getHeight() == 0) {
             return;
         }
+
         Bitmap b = ((BitmapDrawable) drawable).getBitmap();
         Bitmap bitmap = b.copy(Bitmap.Config.ARGB_8888, true);
-
-        int w = getWidth()/*, h = getHeight()*/;
-
-
-        Bitmap roundBitmap = getCroppedBitmap(bitmap, w);
+       // canvas = new Canvas(bitmap.copy(Bitmap.Config.ARGB_8888, true));
+        Bitmap roundBitmap= getCroppedBitmap(bitmap, radius);
         canvas.drawBitmap(roundBitmap, 0, 0, null);
 
+
+
     }
 
-    private static Bitmap getCroppedBitmap(Bitmap bmp, int radius) {
-        Bitmap bitmap;
-        if (bmp.getWidth() != radius || bmp.getHeight() != radius)
-            bitmap = Bitmap.createScaledBitmap(bmp, radius, radius, false);
+    private Bitmap getCroppedBitmap(Bitmap bitmap, int radius) {
+        Bitmap finalBitmap;
+
+        if (bitmap.getWidth() != radius || bitmap.getHeight() != radius)
+            finalBitmap = Bitmap.createScaledBitmap(bitmap, radius, radius,
+                    false);
         else
-            bitmap = bmp;
-        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
-                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+            finalBitmap = bitmap;
+        Bitmap output = Bitmap.createBitmap(finalBitmap.getWidth(),
+                finalBitmap.getHeight(), Bitmap.Config.ARGB_8888);
+
         Canvas canvas = new Canvas(output);
+        final Rect rect = new Rect(0, 0,getWidth(),
+                getHeight());
+        paintCanvas.setColor(Color.WHITE);
+        paintCanvas.setAntiAlias(true);
+        paintBorder.setAntiAlias(true);
 
-//        final int color = 0xffa19774;
-        final Paint paint = new Paint();
-        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        paintCanvas.setFilterBitmap(true);
+        paintBorder.setFilterBitmap(true);
 
-        paint.setAntiAlias(true);
-        paint.setFilterBitmap(true);
-        paint.setDither(true);
+        paintCanvas.setDither(true);
+        paintBorder.setDither(true);
+
+
         canvas.drawARGB(0, 0, 0, 0);
-        paint.setColor(Color.parseColor("#BAB399"));
-        canvas.drawCircle(bitmap.getWidth() / 2 + 0.7f, bitmap.getHeight() / 2 + 0.7f,
-                bitmap.getWidth() / 2 + 0.1f, paint);
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        canvas.drawBitmap(bitmap, rect, rect, paint);
+       // paintCanvas.setColor(Color.parseColor("#BAB399"));
+        //paintBorder.setColor(Color.parseColor("#5f61b6"));
+        paintBorder.setStyle(Paint.Style.STROKE);
+        paintBorder.setStrokeWidth(borderWidth);
 
-
+        canvas.drawCircle(finalBitmap.getWidth() / 2 + 0.7f,
+                finalBitmap.getHeight() / 2 + 0.7f,
+                finalBitmap.getWidth() / 2 + 0.1f, paintCanvas);
+        canvas.drawBitmap(finalBitmap, rect, rect, paintCanvas);
+        paintCanvas.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawCircle(radius / 2 + 0.7f,
+                radius / 2 + 0.7f, radius / 2 - paintBorder.getStrokeWidth()/2 +0.1f, paintBorder);
         return output;
+
     }
+    public void setBorderColor(int borderColor){
+        if (paintBorder != null)
+            paintBorder.setColor(borderColor);
+        this.invalidate();
+
+    }
+    public void setBorderWidth(float borderWidth){
+        this.borderWidth = borderWidth;
+        this.invalidate();
+    }
+    public void drawRing(Canvas canvas,int radius){
+        paintCanvas.setAntiAlias(true);
+        paintBorder.setAntiAlias(true);
+        paintCanvas.setFilterBitmap(true);
+        paintBorder.setFilterBitmap(true);
+        paintCanvas.setDither(true);
+        paintBorder.setDither(true);
+        paintCanvas.setColor(Color.WHITE);
+        paintBorder.setColor(this.getResources().getColor(R.color.friend_req_color));
+        paintBorder.setStyle(Paint.Style.STROKE);
+        paintBorder.setStrokeWidth(borderWidth);
+        canvas.drawARGB(0, 0, 0, 0);
+        canvas.drawCircle(getWidth() / 2 + 0.7f,
+                getHeight() / 2 + 0.7f,
+                getWidth() / 2 + 0.1f, paintCanvas);
+        paintCanvas.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawCircle(radius / 2 + 0.7f,
+                radius / 2 + 0.7f, radius / 2 - paintBorder.getStrokeWidth()/2 +0.1f, paintBorder);
+    }
+
 
 }
